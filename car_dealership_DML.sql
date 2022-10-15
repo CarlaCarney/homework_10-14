992b07ff150cdd0721ccce00c87c07f0195c249a
Carla Carney <carlacarney2@gmail.com>
2022-10-15 17:59:13 -0300

    weekend project

---
+++ car_dealership_DML.sql
@@ -0,0 +1,26 @@
+INSERT INTO employees VALUES(10, 'Ross', 'Geller', 'Manager');
+INSERT INTO employees VALUES(11, 'Phoebe', 'Buffay', 'Salesperson');
+INSERT INTO employees VALUES(12, 'Joseph', 'Tribbiani', 'mechanic');
+INSERT INTO employees VALUES(13, 'Gunther', 'CentralPerk', 'mechanic');
+INSERT INTO employees VALUES(14, 'Frank', 'Jr', 'Salesperson');
+
+
+INSERT INTO customer VALUES(20, 'Rachel', 'Green', '203-123-4567', 'rachelqueen@gmail.com');
+INSERT INTO customer VALUES(21, 'Monica', 'Geller', '751-987-6543', 'lovecleaning@gmail.com');
+INSERT INTO customer VALUES(22, 'Chandler', 'Bing', '888-123-4342', 'chandlermurielbing@gmail.com');
+INSERT INTO customer VALUES(23, 'Janice', 'Dickinson', '234-234-2145', 'ohmygod@gmail.com');
+
+
+INSERT INTO car VALUES(30, 'Subaru', 'WRX', False);
+INSERT INTO car VALUES(31, 'Subaru', 'BRZ', True);
+INSERT INTO car VALUES(32, 'Subaru', 'Forester', False);
+INSERT INTO car VALUES(33, 'Subaru', 'Outback', False);
+INSERT INTO car VALUES(34, 'Subaru', 'XV', True);
+
+
+INSERT INTO invoice VALUES(40, 11, 21, 34);
+INSERT INTO invoice VALUES(41, 14, 23, 33);
+
+
+INSERT INTO service_ticket VALUES(50, 13, 20, 32, 'Battery replacement, tire rotation.');
+INSERT INTO service_ticket VALUES(51, 12, 23, 33, 'Oil change.');
